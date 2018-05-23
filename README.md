3924a314022af1a20e8bdb7cb1d9d14f5193d67c
Leah Joyce <leahpjoyce@gmail.com>
2018-05-22 21:55:01 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Memory Game Project
+
+This project is a demonstration of my skilss in HTML, CSS, JavaScript.
+Its based on the <a href="https://review.udacity.com/#!/rubrics/591/view">Memory Game project rubric
+</a>
---
+++ js/app.js
@@ -0,0 +1,78 @@
+/*
+ * Create a list that holds all of your cards
+ */
+
+const listOfCards = [
+    'fa-diamond',
+    'fa-paper-plane-o',
+    'fa-anchor',
+    'fa-bolt',
+    'fa-cube',
+    'fa-leaf',
+    'fa-bicycle',
+    'fa-bomb'
+    
+];
+
+
+
+const deck = document.querySelector('.deck');
+const card = document.getElementsByClassName("card");
+
+
+
+function openCard(event) {
+   
+   event.target.classList.add('open', 'show');
+
+}
+
+deck.addEventListener("click", openCard);
+
+
+
+//```
+//const mainHeading = document.querySelector('h1');
+//
+//mainHeading.addEventListener('click', function () {
+//  console.log('The heading was clicked!');
+//});
+//
+//```
+
+
+
+/*
+ * Display the cards on the page
+ *   - shuffle the list of cards using the provided "shuffle" method below
+ *   - loop through each card and create its HTML
+ *   - add each card's HTML to the page
+ */
+
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+
+
+/*
+ * set up the event listener for a card. If a card is clicked:
+ *  - display the card's symbol (put this functionality in another function that you call from this one)
+ *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
+ *  - if the list already has another card, check to see if the two cards match
+ *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
+ *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
+ *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
+ *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
+ */
