62c1dbc4007e5e2afbe331d76789729b7d122fcf
Leah Joyce <leahpjoyce@gmail.com>
2018-05-25 15:39:19 -0500

    test: use arrayCards to generate <li> elements

--- js/app.js
+++ js/app.js
@@ -15,39 +15,81 @@ let arrayCards = [
 ];
 
 
-
+let allCards = document.querySelectorAll('.card');
 let openCards = [];
 
+function generateCard(card) {
+    return `<li class="card"><i class="fa ${card}"></i></li>`;
+}
 
-let allCards = document.querySelectorAll('.card'); // get the card class and store to allCards
 
-allCards.forEach(function(event) {
-    event.addEventListener('click', function(e) {
-        event.classList.add('open', 'show');
-        openCards.push(event);
-        
-        if(openCards.length >= 2) {
-            setTimeout(function() {
-                openCards.forEach(function(event) {
-                    event.classList.remove('open', 'show');
-                
-            });
-            }, 2000);
-        } else {
-            openCards.push(event);
-            event.classList.add('open', 'show');
-            
-        }
-        
+const deck = document.querySelector('.deck');
+
+function initGame() {
+    var getCard = arrayCards.map(function(card) {
+        return generateCard(card);
     });
-    
-});
+    deck.innerHTML = getCard.join('');
+}
+
+initGame();
 
 
 
+allCards.forEach(function(card) {  // Test Code
+    card.addEventListener('click', function() {
+        if(!card.classList.contains('open') && !card.classList.contains('show') && !card.classList.contains('open')) {
+            openCards.push(card);
+            card.classList.add('open','show');
+        
+            if (openCards.length == 2) {
+                if(openCards[0].dataset.card == openCards[1].dataset.card){
+                        openCards[0].classList.add('match');
+                        openCards[0].classList.add('open');
+                        openCards[0].classList.add('show');
 
+                        openCards[1].classList.add('match');
+                        openCards[1].classList.add('open');
+                        openCards[1].classList.add('show');
 
+                        openCards[2].classList.add('match');
+                        openCards[2].classList.add('open');
+                        openCards[2].classList.add('show');
 
+                        openCards[3].classList.add('match');
+                        openCards[3].classList.add('open');
+                        openCards[3].classList.add('show');
+
+                        openCards[4].classList.add('match');
+                        openCards[4].classList.add('open');
+                        openCards[4].classList.add('show');
+
+                        openCards[5].classList.add('match');
+                        openCards[5].classList.add('open');
+                        openCards[5].classList.add('show');
+
+                        openCards[6].classList.add('match');
+                        openCards[6].classList.add('open');
+                        openCards[6].classList.add('show');
+
+                        openCards[7].classList.add('match');
+                        openCards[7].classList.add('open');
+                        openCards[7].classList.add('show');
+
+                        openCards = [];
+                  } else {
+                        setTimeout(function() {
+                            openCards.forEach(function(card) {
+                                card.classList.remove('open','show');
+                            });
+                            openCards = [];
+                    }, 100);
+
+                }
+            } 
+        }
+            
+    });
 
 
 /*
